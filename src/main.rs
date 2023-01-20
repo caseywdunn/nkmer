fb888ffa6b902ebeb5df4e8ac9cdd2442fc36769
Casey Dunn <casey.dunn@yale.edu>
2023-01-20 12:32:22 -0500

    creates kmers for forward strand

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,50 @@
+use std::array;
 use std::io;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 use flate2::read::GzDecoder;
 
+use std::collections::HashMap;
+
+const PARTS : u32 = 100;
+const K : u32 = 21;
+
+fn string2kmers (seq : String) -> Vec<u64> {
+
+    // kmers are encoded as u64, with two bits per base
+    // A = 00, C = 01, G = 10, T = 11
+
+    // Create a mask that has 1s in the last 2*K bits
+    let mask : u64 = (1 << (2*K)) - 1;
+
+    let mut kmers : Vec<u64> = Vec::new();
+    let mut frame : u64 = 0;
+    let mut n_valid = 0; // number of valid bases in the frame
+    for (i, c) in seq.chars().enumerate() {
+        let base = match c {
+            'A' => 0,
+            'C' => 1,
+            'G' => 2,
+            'T' => 3,
+            'N' => 4,
+            _ => 5,
+        };
+        if (base < 4) {
+            frame = (frame << 2) | base;
+            n_valid += 1;
+            if (n_valid >= K) {
+                kmers.push(frame & mask);
+            }
+        } else if (base==4) {
+            frame = 0;
+            n_valid = 0;
+        } else {
+            panic!("Invalid base: {}", c);
+        }
+    }
+    return kmers;
+}
 fn main() {
 
     // Ingest all command line arguments as input files
@@ -44,6 +85,23 @@ fn main() {
         n_bases_all += n_bases;
     }
 
-
     println!("Total: records {}, bases {}", n_records_all, n_bases_all);
+
+
+
+    let start = std::time::Instant::now();
+
+    let mut kmers : Vec<u64> = Vec::new();
+    for seq in seqs {
+        // Call string2kmers on each sequence and append the result to kmers
+        kmers.append(&mut string2kmers(seq));
+    }
+
+    println!("Total kmers: {}", kmers.len());
+
+    let stop = std::time::Instant::now();
+
+    println!("Time: {} ms", (stop - start).as_millis());
+
+
 }
